45419cc3b6e51483a921c92aea62ea6135aeeb19
Rahul Sheth <rahul@snap.com>
2021-03-16 00:53:15 -0400

    git subrepo clone (merge) https://github.com/Bitmoji/GLTFUtility SDKs/Unity/Plugins/GLTFUtility
    subrepo:
      subdir:   "SDKs/Unity/Plugins/GLTFUtility"
      merged:   "286fab5"
    upstream:
      origin:   "https://github.com/Bitmoji/GLTFUtility"
      branch:   "master"
      commit:   "286fab5"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

---
+++ SDKs/Unity/Plugins/GLTFUtility/Scripts/Settings/ImportSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+
+namespace Bitmoji.GLTFUtility {
+	[Serializable]
+	public class ImportSettings {
+
+		public bool materials = true;
+		[FormerlySerializedAs("shaders")]
+		public ShaderSettings shaderOverrides = new ShaderSettings();
+		public AnimationSettings animationSettings = new AnimationSettings();
+		public bool generateLightmapUVs;
+
+		[Tooltip("Script used to process extra data.")]
+		public GLTFExtrasProcessor extrasProcessor;
+
+	}
+}
